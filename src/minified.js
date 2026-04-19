63a2cf15f03fe344be27800dfac626a9276eb89f
ObiloxYT <AfriaStudios@outlook.in>
2026-04-19 21:41:01 +0530

    Enable experimental retries and update event handling

--- src/minified.js
+++ src/minified.js
@@ -5,13 +5,15 @@ SupportedEvents:
 /**Times to try. Do not set below 1 or else it wont work.*/
 EventTries:1,
 /**Whether to turn on Experimental retries.*/
-ExperimentalRetries:!1};let _={};for(let e=0;e<CONFIG.SupportedEvents.length;e++)_[CONFIG.SupportedEvents[e]]=[];
+ExperimentalRetries:!0,
+/**What to do when code gets retried. @type{"RetryFn"|"NextFn"}*/
+OnRetryPolicy:"NextFn"};let _={};for(let t=0;t<CONFIG.SupportedEvents.length;t++)_[CONFIG.SupportedEvents[t]]=[];
 /**@type{Record<typeof CONFIG["SupportedEvents"][number],Function[]>}*/let Events=_;const EventsManager={
 /**
     * @template {typeof CONFIG["SupportedEvents"][number]} NAME
     * @param {NAME} name 
     * @param {globalThis[NAME]} callback
     */
-register(e,t){Events[e].push(t)},
-/** @param {typeof CONFIG["SupportedEvents"][number]} name */
-__startFor(e){globalThis[e]=(...t)=>{const r=Events[e];for(let s=0;s<r.length;s++){const o=r[s];let n=1;if(CONFIG.ExperimentalRetries&&Object.defineProperty(globalThis.InternalError.prototype,"name",{configurable:!0,get:()=>{++n>CONFIG.EventRetries?api.broadcastMessage(`Error at ${e}: Too many retries!`,{color:"#ff9d87"}):o(...t)}}),n>CONFIG.EventRetries&&CONFIG.ExperimentalRetries)return void api.broadcastMessage(`Error at ${e}: Too many retries!`,{color:"#ff9d87"});try{o(...t)}catch(t){api.broadcastMessage(`Error at ${e}: ${t}`,{color:"#ff9d87"})}}}},startAll(){for(let e=0;e<CONFIG.SupportedEvents.length;e++)this.__startFor(CONFIG.SupportedEvents[e])}};globalThis.LOOM=function(){let e={...globalThis};for(let t=0;t<CONFIG.SupportedEvents.length;t++){const r=CONFIG.SupportedEvents[t];let s;Object.defineProperty(e,r,{configurable:!0,set:e=>{s=e,EventsManager.register(r,e)},get:()=>s})}return e},EventsManager.startAll();
+register(t,e){Events[t].push(e)},__runFor(t,e=0,r){const n=Events[t];if(!(e>=n.length))for(let s=e;s<n.length;s++){const e=n[s];let o=0;CONFIG.ExperimentalRetries&&Object.defineProperty(globalThis.InternalError.prototype,"name",{configurable:!0,get:()=>{"NextFn"!==CONFIG.OnRetryPolicy?++o>CONFIG.EventRetries&&"RetryFn"===CONFIG.OnRetryPolicy?api.broadcastMessage("[LOOM] Max retries hit at "+t,{color:"#ff9d87"}):e(...r):this.__runFor(t,s+1,r)}});try{e(...r)}catch(e){if("LOOM_SKIP"===e.message)return;if(api.broadcastMessage(`[LOOM] Error at ${t}: ${e}`,{color:"#ff9d87"}),"NextFn"===CONFIG.OnRetryPolicy)continue}}},
+/** @param {typeof CONFIG["SupportedEvents"][number]} name*/
+__startFor(t){globalThis[t]=(...e)=>{this.__runFor(t,0,e)}},startAll(){for(let t=0;t<CONFIG.SupportedEvents.length;t++)this.__startFor(CONFIG.SupportedEvents[t])}};globalThis.LOOM=function(){let t={...globalThis};for(let e=0;e<CONFIG.SupportedEvents.length;e++){const r=CONFIG.SupportedEvents[e];let n;Object.defineProperty(t,r,{configurable:!0,set:t=>{n=t,EventsManager.register(r,t)},get:()=>n})}return t},EventsManager.startAll();
